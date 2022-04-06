a0370003973f91c25639311cff4defcbf26effe4
youminlim <lim.ym5327@gmail.com>
2022-04-06 23:35:06 +0100

    Updated the code to be able to perform linear regression on a specific set of data

--- linear_regression.py
+++ linear_regression.py
@@ -2,29 +2,57 @@
 
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+X = pd.read_csv("machine_learning/data_file/data.csv")
+labels = ['Re', 'Cd']
+
+x = X[labels[0]]
+x = pd.DataFrame(x)
+y = X[labels[1]]
+y = pd.DataFrame(y)
+y = y.to_numpy()
+
+### Tuning parameters
+theta_initial = np.ones([2,1])
+alpha = 0.11    # Learning rate / %
+N = 100         # Number of gradient iterations
+
+### Cost Function algorithm
+m = len(y)
+initial_vals = pd.DataFrame(np.ones([m,1]))
+x = initial_vals.assign(Re = x)
+x = x.to_numpy()
+diff = np.zeros(m)
+
+### Gradient Descent algorithm
+J_hist = np.zeros([N,1])
+
+for j in range(100):
+    for i in range(m):
+        diff[i] = np.dot(x[i],theta_initial) - y[i]
+    # Calculating the cost function
+    J = (1/(2*m)) * np.dot(np.transpose(diff),diff)
+    # Calculating the gradient
+    G = (1/m) * np.dot(np.transpose(x),diff)
+    # Updating theta
+    theta_initial = theta_initial.transpose() - (alpha * G)
+    theta_initial = theta_initial.transpose()
+    # Saving the cost function
+    J_hist[j] = J
     
-def GD(X, y, theta, alpha, N):
-    # performs gradient descent 
-    
-    m = len(y)
-    # Rewrites X to contain initial values for theta0
-    X = [np.ones(m,1), X]
-    # Initialise histories of cost function
-    J_hist = np.zeros(N,1)
-    
-    # Computes the cost function
-    J = (1/2*m) * np.transpose(X*theta - y) * (X*theta - y)
-    # Computes the gradient
-    G = (1/m) * np.transpose(X) * (X*theta - y)
-    
-    # Performing gradient descent
-    for j in range(1,N+1):
-        # Updating theta
-        theta = theta - (alpha * G)
-        # Save the cost function values
-        J_hist(j) == J
-        
-    return [theta, J_hist]
-        
-X = pd.read_csv(data.csv)
+fig, ax = plt.subplots()
+ax.plot(J_hist)
+plt.show()
+
+# Plot of regressed line
+x_r = np.linspace(-1,7,100)
+y_r = pd.DataFrame(np.ones([len(x_r),1]))
+y_r = y_r.assign(x = x_r)
+y_r = np.dot(y_r.to_numpy(),theta_initial)
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+ax.plot(x_r,y_r)
+plt.show()
+
