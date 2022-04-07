7d80bdd8317568a5ab92720d59d8bd870ccf42aa
youminlim <lim.ym5327@gmail.com>
2022-04-07 11:35:21 +0100

    GD function now working

--- GD.py
+++ GD.py
@@ -1,4 +1,6 @@
-#%% Linear Regression Function
+# Gradient Descent Function
+# The output theta should be used to calculate the start 
+# and end points of the regression line
 
 def GD(X, y, theta, alpha, N):
     # performs gradient descent 
@@ -9,17 +11,18 @@ def GD(X, y, theta, alpha, N):
     initial_vals = pd.DataFrame(np.ones([m,1]))
     X = initial_vals.assign(x = X)
     X = X.to_numpy()
+    
     # Initialise histories of cost function and diff
     J_hist = np.zeros([N,1])
-    diff = np.zeros([N,1])
+    diff = np.zeros(m)
     
     for j in range(N):
         for i in range(m):
-            diff[i] = np.dot(x[i],theta) - y[i]
+            diff[i] = np.dot(X[i],theta) - y[i]
         # Calculating the cost function
         J = (1/(2*m)) * np.dot(np.transpose(diff),diff)
         # Calculating the gradient
-        G = (1/m) * np.dot(np.transpose(x),diff)
+        G = (1/m) * np.dot(np.transpose(X),diff)
         # Updating theta
         theta = theta.transpose() - (alpha * G)
         theta = theta.transpose()
